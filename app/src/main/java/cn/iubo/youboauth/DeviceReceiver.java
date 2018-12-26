e4a768a8d38245e4f556b1bac42d88e466f130a0
zxlworking <szzhangxiaolong@163.com>
2018-12-26 19:30:49 +0800

    add license document

---
+++ app/src/main/java/cn/iubo/youboauth/DeviceReceiver.java
@@ -0,0 +1,27 @@
+package cn.iubo.youboauth;
+
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class DeviceReceiver extends DeviceAdminReceiver {
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        return context.getString(R.string.disable_warning);
+    }
+
+}
